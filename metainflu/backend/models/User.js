493cd85585df54b9394752241b1b9b4e58509415
abh1hi <162477075+abh1hi@users.noreply.github.com>
2025-11-07 13:03:31 +0530

    Add password field to User model for dual authentication

--- metainflu/backend/models/User.js
+++ metainflu/backend/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema(
   {
@@ -16,6 +17,11 @@ const userSchema = mongoose.Schema(
       unique: true,
       sparse: true, // Allows null values to not violate unique constraint
     },
+    password: {
+      type: String,
+      // Not required to maintain backward compatibility with OTP-only users
+      select: false, // Don't include password in queries by default
+    },
     otp: String,
     otpExpires: Date,
     role: {
@@ -29,5 +35,21 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password') || !this.password) {
+    next();
+  } else {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  }
+});
+
+// Method to compare entered password with hashed password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
