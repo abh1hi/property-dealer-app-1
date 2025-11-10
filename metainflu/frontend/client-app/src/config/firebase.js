dbe8fbe5efe72009b33f8aab235d7b74d1c00994
abhinavbhatnaria5 <abhinavbhatnaria5@gmail.com>
2025-11-10 21:19:36 +0000

    deploy ready

--- metainflu/frontend/client-app/src/config/firebase.js
+++ metainflu/frontend/client-app/src/config/firebase.js
@@ -1,21 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth, connectAuthEmulator } from "firebase/auth";
 import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAA1WrAcjvokL4q6f208RLIwqzhxXoSS3g",
-  authDomain: "metainflu.firebaseapp.com",
-  projectId: "metainflu",
-  storageBucket: "metainflu.appspot.com",
-  messagingSenderId: "1083446985532",
-  appId: "1:1083446985532:web:9852239d1b6b5f4749f7a7",
-  measurementId: "G-1W5L3PJMPE"
+  authDomain: "test1-50da1.firebaseapp.com",
+  projectId: "test1-50da1",
+  storageBucket: "test1-50da1.firebasestorage.app",
+  messagingSenderId: "317809189734",
+  appId: "1:317809189734:web:4177473e2f9143010a5ea7",
+  measurementId: "G-1K9VKFNNWM"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Initialize services
 const auth = getAuth(app);
