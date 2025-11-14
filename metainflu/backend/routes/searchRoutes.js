20639855243bbcf2b3d054d111db1984919fc5bb
abh1hi <abhinavbhatnaria5@gmail.com>
2025-11-15 00:41:45 +0530

    removed backend

--- metainflu/backend/routes/searchRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { searchProperties } = require('../controllers/propertyController');
-
-router.route('/').get(searchProperties);
-
-module.exports = router;
